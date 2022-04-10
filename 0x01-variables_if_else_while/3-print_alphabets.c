6c50a25da291358692ebffaf292faca7370812fb
Munezero2000 <101561925+Munezero2000@users.noreply.github.com>
2022-04-11 01:18:43 +0200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - Entry point
- * Description: prints the alphabet in lowercase\n
- * Return: Always 0 (success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int ch;
+int x, y;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		putchar(ch);
-	}
-	putchar('\n');
+<<<<<<< HEAD
+for (m = 97; m <= 122; m++)
+putchar(m);
+for (n = 65; n <= 90; n++)
+putchar(n);
+=======
+for (x = 97; x<= 122; x++)
+{
+putchar(x);
+}
+for (y = 65; y <= 90; y++)
+{
+putchar(y);
+}
+>>>>>>> c38d008b18676f16774155151031a0fcc1c46752
+putchar('\n');
 return (0);
 }

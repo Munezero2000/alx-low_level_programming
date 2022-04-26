4b5dc07a24721d7a3283e683c0af0dfd749b1551
Munezero2000 <101561925+Munezero2000@users.noreply.github.com>
2022-04-26 13:40:40 +0200

    0-memset task commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _memeset - 
+ * @n bytes of memory of area to fill
+ * @b constat bytes to fill into 
+ * @s bytes of memory to be filled 
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int c;
+	
+	for (c = 0; c < n; c++);
+	s[c]= b;
+	return (0);
+}
+ 
+
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+into this directory there is more about c pointers arrays and strings 

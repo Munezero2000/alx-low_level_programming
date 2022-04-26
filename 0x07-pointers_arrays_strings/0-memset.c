d5b3a6959cd48b7cfa52180bf965e90bfacef6c3
Munezero2000 <101561925+Munezero2000@users.noreply.github.com>
2022-04-26 23:28:48 +0200

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,6 +8,7 @@
   *
   * Return: the memory area filled
   */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int a;
@@ -16,6 +17,6 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		s[a] = b;
 	}
-
 	return (s);
+
 }
